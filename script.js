22c632971a3f896888d9b8a00d0e793685ee7587
Alixyen <101437709+Alixyen@users.noreply.github.com>
2024-05-09 12:22:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+  var tintin ={
+   name: "Tintin",
+   age: 14,
+};
+
+  var snowy ={
+   name: "Snowy",
+   age: 9,
+};
+
+var captainHaddock ={
+   name: "Captain Haddock",
+   age: 70,
+};
+
+var professorCalculus ={
+   name: "Professor Calculus",
+   age: 60,
+};
+
+var thompsonAndThompson ={
+   name: "Thompson and Thompson",
+   age: 40,
+};
+
+var biancaCastofiere ={
+   name: "Bianca Castofiere",
+   age: 45, 
+};
+
+var rastaPopoulos ={
+   name: "Rasta Popoulos",
+   age: 43,
+};
+
+var nestor ={
+   name: "Nestor",
+   age: 46,
+};
+
+
+function maxage() {
+  var a = Math.max(14, 9, 70, 60, 40, 45, 43, 46);
+  var x = a 
+  document.getElementById("maxage").innerHTML = x;
+}
+
+
